b6f126fb95bee42dec60ad865fcde3c523aa0fe0
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-11-19 12:21:51 +0100

    removed invisible bad chars from magnitudeenergy.yaml

--- sod/stream2segment/configs/magnitudeenergy.py
+++ sod/stream2segment/configs/magnitudeenergy.py
@@ -258,10 +258,19 @@ def _main(segment, config, raw_trace, inventory_used):
     for period, psdval in zip(required_psd_periods, required_psd_values):
         ret['psd@%ssec' % str(period)] = float(psdval)
 
-    ret['outlier'] = segment.station.id in config['bad']
-    # ret['suspect'] = segment.station.id in config['suspect']
-    ret['subclass'] = 'may.be.outlier' if segment.station.id in config['suspect'] \
-        else 'unlabeled' if segment.station.id not in config['good'] else ''
+    if segment.station.id in config['bad']:
+        ret['outlier'] = True
+        ret['subclass'] = ''
+    elif segment.station.id in config['good']:
+        ret['outlier'] = False
+        ret['subclass'] = ''
+    elif segment.station.id in config['suspect']:
+        ret['outlier'] = False
+        ret['subclass'] = 'unlabeled.maybe.outlier'
+    else:
+        ret['outlier'] = False
+        ret['subclass'] = 'unlabeled.unknown'
+
         # ret['modified'] = ''
         # ret['window_type'] = window_type
         # ret['start_time'] = raw_trace.stats.starttime.datetime
