52f97731c88896acfb3b88d479584a35997f2bb5
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2020-06-13 14:48:14 +0200

    implementing ppsd optimized

--- sod/scripts/ppsd_calculator.py
+++ sod/scripts/ppsd_calculator.py
@@ -1,62 +1,74 @@
-import numpy as np
-from obspy.signal.spectral_estimation import PPSD, dtiny
-from obspy.core.stream import read
-from os.path import dirname, join
-from obspy.core.inventory.inventory import read_inventory
-from matplotlib import mlab
+'''
+Implements an optimized version of the power spectral density (PSD) function
+of the PPSD module of ObsPy. The function is the feature extractor for our
+machine learning model (Ifsolation Forest) for anomaly detection in segments
+amplitudes 
+'''
 import math
 import time
+import numpy as np
+from matplotlib import mlab
+from obspy.signal.util import prev_pow_2
+from obspy.signal.spectral_estimation import PPSD, dtiny, fft_taper
+from obspy.core.stream import read
+from obspy.core.inventory import Inventory
+from obspy.core.inventory.inventory import read_inventory
 
 
-def psd_values(periods, raw_trace, inventory):
-    periods = np.asarray(periods)
-    try:
-        ppsd_ = psd(raw_trace, inventory)
-    except Exception as esc:
-        raise ValueError('%s error when computing PSD: %s' %
-                         (esc.__class__.__name__, str(esc)))
-    # check first if we can interpolate ESPECIALLY TO SUPPRESS A WEIRD
-    # PRINTOUT (numpy?): something like '5064 5062' which happens
-    # on IndexError (len(ppsd_.psd_values)=0)
-    if not len(ppsd_.psd_values):
-        raise ValueError('Expected 1 psd array, no psd computed')
-    val = np.interp(
-        np.log10(periods),
-        np.log10(ppsd_.period_bin_centers),
-        ppsd_.psd_values[0]
-    )
-    val[periods < ppsd_.period_bin_centers[0]] = np.nan
-    val[periods > ppsd_.period_bin_centers[-1]] = np.nan
-    return val
+class old:
+    '''container for the old functions used in the paper'''
+
+    @staticmethod
+    def psd_values(periods, raw_trace, inventory):
+        periods = np.asarray(periods)
+        try:
+            ppsd_ = old.psd(raw_trace, inventory)
+        except Exception as esc:
+            raise ValueError('%s error when computing PSD: %s' %
+                             (esc.__class__.__name__, str(esc)))
+        # check first if we can interpolate ESPECIALLY TO SUPPRESS A WEIRD
+        # PRINTOUT (numpy?): something like '5064 5062' which happens
+        # on IndexError (len(ppsd_.psd_values)=0)
+        if not len(ppsd_.psd_values):
+            raise ValueError('Expected 1 psd array, no psd computed')
+        val = np.interp(
+            np.log10(periods),
+            np.log10(ppsd_.period_bin_centers),
+            ppsd_.psd_values[0]
+        )
+        val[periods < ppsd_.period_bin_centers[0]] = np.nan
+        val[periods > ppsd_.period_bin_centers[-1]] = np.nan
+        return val
+
+    @staticmethod
+    def psd(raw_trace, inventory):
+        # tr = segment.stream(True)[0]
+        dt = raw_trace.stats.endtime - raw_trace.stats.starttime  # total_seconds
+        ppsd = PPSD(raw_trace.stats, metadata=inventory, ppsd_length=int(dt))
+        ppsd.add(raw_trace)
+        return ppsd
 
 
-def psd(raw_trace, inventory):
-    # tr = segment.stream(True)[0]
-    dt = raw_trace.stats.endtime - raw_trace.stats.starttime  # total_seconds
-    ppsd = PPSD(raw_trace.stats, metadata=inventory, ppsd_length=int(dt))
-    ppsd.add(raw_trace)
-    return ppsd
-
-
-if __name__ == "__main__":
-    trace, inv = 'trace_GE.APE.mseed', 'inventory_GE.APE.xml'
-    stream = read(join(dirname(__file__), 'miniseed', trace))
-    inv = read_inventory(join(dirname(__file__), 'miniseed', inv))
-    t = time.time()
-    psd_values([5], stream[0], inv)
-    
-    
-    
-def __process(tr, metadata, special_handling=None):
+def psd_values(psd_periods, tr, metadata, special_handling=None,
+               period_smoothing_width_octaves=1.0,
+               period_step_octaves=0.125):
     """
-    Processes a segment of data and save the psd information.
-    Whether `Trace` is compatible (station, channel, ...) has to
-    checked beforehand.
+    Calculates the power spectral density (psd) of the given
+    trace `tr`, and returns the values in dB at the given `psd_periods`.
 
-    :type tr: :class:`~obspy.core.trace.Trace`
-    :param tr: Compatible Trace with data of one PPSD segment
-    :returns: `True` if segment was successfully processed,
-        `False` otherwise.
+    Note: all optional parameters should be left as they are,
+        as the given parameter where those used for training.
+        For any further information, see
+        :class:`~obspy.signal.spectral_estimation.PPSD` and
+        :class:`~obspy.signal.spectral_estimation.PPSD.__process`
+
+    :psd_periods: numeric list/array of periods (in second)
+    :param tr: obspy Trace
+    :param metadata: Response information of instrument. It must be
+        a :class:`~obspy.core.inventory.inventory.Inventory` (e.g. read from a
+        StationXML file using
+        :func:`~obspy.core.inventory.inventory.read_inventory` or fetched
+        from a :mod:`FDSN <obspy.clients.fdsn>` webservice)
     """
 #     # XXX DIRTY HACK!!
 #     if len(tr) == self.len + 1:
@@ -78,6 +90,26 @@ def __process(tr, metadata, special_handling=None):
     except AttributeError:
         pass
 
+    # added stuff by me:
+    ppsd_length = tr.stats.endtime - tr.stats.starttime  # float, seconds
+    stats = tr.stats
+    sampling_rate = stats.sampling_rate
+    # calculate derived attributes
+    # nfft is determined mimicking the fft setup in McNamara&Buland
+    # paper:
+    # (they take 13 segments overlapping 75% and truncate to next lower
+    #  power of 2)
+    #  - take number of points of whole ppsd segment (default 1 hour)
+    nfft = ppsd_length * sampling_rate
+    #  - make 13 single segments overlapping by 75%
+    #    (1 full segment length + 25% * 12 full segment lengths)
+    nfft = nfft / 4.0
+    #  - go to next smaller power of 2 for nfft
+    nfft = prev_pow_2(nfft)
+    #  - use 75% overlap
+    #    (we end up with a little more than 13 segments..)
+    nlap = int(0.75 * nfft)
+
     # restitution:
     # mcnamara apply the correction at the end in freq-domain,
     # does it make a difference?
@@ -85,13 +117,14 @@ def __process(tr, metadata, special_handling=None):
     # Yes, you should avoid removing the response until after you
     # have estimated the spectra to avoid elevated lp noise
 
-    spec, _freq = mlab.psd(tr.data, self.nfft, self.sampling_rate,
+    spec, _freq = mlab.psd(tr.data, nfft, sampling_rate,
                            detrend=mlab.detrend_linear, window=fft_taper,
-                           noverlap=self.nlap, sides='onesided',
+                           noverlap=nlap, sides='onesided',
                            scale_by_freq=True)
 
     # leave out first entry (offset)
     spec = spec[1:]
+    freq = _freq[1:]
 
     # working with the periods not frequencies later so reverse spectrum
     spec = spec[::-1]
@@ -99,15 +132,15 @@ def __process(tr, metadata, special_handling=None):
     # Here we remove the response using the same conventions
     # since the power is squared we want to square the sensitivity
     # we can also convert to acceleration if we have non-rotational data
-    if self.special_handling == "ringlaser":
+    if special_handling == "ringlaser":
         # in case of rotational data just remove sensitivity
-        spec /= self.metadata['sensitivity'] ** 2
+        spec /= metadata['sensitivity'] ** 2
     # special_handling "hydrophone" does instrument correction same as
     # "normal" data
     else:
         # determine instrument response from metadata
         try:
-            resp = self._get_response(tr)
+            resp = _get_response(tr, metadata, nfft)
         except Exception as e:
             msg = ("Error getting response from provided metadata:\n"
                    "%s: %s\n"
@@ -122,11 +155,11 @@ def __process(tr, metadata, special_handling=None):
         # Now get the amplitude response (squared)
         respamp = np.absolute(resp * np.conjugate(resp))
         # Make omega with the same conventions as spec
-        w = 2.0 * math.pi * _freq[1:]
+        w = 2.0 * math.pi * freq
         w = w[::-1]
         # Here we do the response removal
         # Do not differentiate when `special_handling="hydrophone"`
-        if self.special_handling == "hydrophone":
+        if special_handling == "hydrophone":
             spec = spec / respamp
         else:
             spec = (w ** 2) * spec / respamp
@@ -138,13 +171,158 @@ def __process(tr, metadata, special_handling=None):
     spec = np.log10(spec)
     spec *= 10
 
-    smoothed_psd = []
+    psd_periods = np.asarray(psd_periods)
+    smoothed_psd, period_bin_centers = [], []
+    _psd_periods = 1.0 / freq[::-1]
+    period_limits = (_psd_periods[0], _psd_periods[-1])
+    for periods_bins in \
+            _setup_yield_period_binning(psd_periods,
+                                        period_smoothing_width_octaves,
+                                        period_step_octaves, period_limits):
+        period_bin_left, period_bin_center, period_bin_right = periods_bins
+        _spec_slice = spec[(period_bin_left <= _psd_periods) &
+                           (_psd_periods <= period_bin_right)]
+        smoothed_psd.append(_spec_slice.mean())
+        period_bin_centers.append(period_bin_center)
+
+    val = np.interp(
+        np.log10(psd_periods),
+        np.log10(period_bin_centers),
+        smoothed_psd
+    )
+    val[psd_periods < period_bin_centers[0]] = np.nan
+    val[psd_periods > period_bin_centers[-1]] = np.nan
+    return val
+
+
+#     smoothed_psd = []
+#     # do this for the whole period range and append the values to our lists
+#     for per_left, per_right in zip(self.period_bin_left_edges,
+#                                    self.period_bin_right_edges):
+#         specs = spec[(per_left <= self.psd_periods) &
+#                      (self.psd_periods <= per_right)]
+#         smoothed_psd.append(specs.mean())
+#     smoothed_psd = np.array(smoothed_psd, dtype=np.float32)
+#     self.__insert_processed_data(tr.stats.starttime, smoothed_psd)
+#     return True
+
+
+def _get_response(tr, metadata, nfft):
+    # check type of metadata and use the correct subroutine
+    # first, to save some time, tried to do this in __init__ like:
+    #   self._get_response = self._get_response_from_inventory
+    # but that makes the object non-picklable
+    if isinstance(metadata, Inventory):
+        return _get_response_from_inventory(tr, metadata, nfft)
+#         elif isinstance(self.metadata, Parser):
+#             return self._get_response_from_parser(tr)
+#         elif isinstance(self.metadata, dict):
+#             return self._get_response_from_paz_dict(tr)
+#         elif isinstance(self.metadata, (str, native_str)):
+#             return self._get_response_from_resp(tr)
+#     else:
+#         msg = "Unexpected type for `metadata`: %s" % type(self.metadata)
+#         raise TypeError(msg)
+    msg = "Unexpected type for `metadata`: %s" % type(metadata)
+    raise TypeError(msg)
+
+
+def _get_response_from_inventory(tr, metadata, nfft):
+    inventory = metadata
+    delta = 1.0 / tr.stats.sampling_rate
+    id_ = "%(network)s.%(station)s.%(location)s.%(channel)s" % tr.stats
+    response = inventory.get_response(id_, tr.stats.starttime)
+    resp, _ = response.get_evalresp_response(t_samp=delta, nfft=nfft,
+                                             output="VEL")
+    return resp
+
+
+def _setup_yield_period_binning(psd_periods, period_smoothing_width_octaves,
+                                period_step_octaves, period_limits):
+    """
+    Set up period binning.
+    """
+    if period_limits is None:
+        period_limits = (psd_periods[0], psd_periods[-1])
+    # we step through the period range at step width controlled by
+    # period_step_octaves (default 1/8 octave)
+    period_step_factor = 2 ** period_step_octaves
+    # the width of frequencies we average over for every bin is controlled
+    # by period_smoothing_width_octaves (default one full octave)
+    period_smoothing_width_factor = \
+        2 ** period_smoothing_width_octaves
+    # calculate left/right edge and center of first period bin
+    # set first smoothing bin's left edge such that the center frequency is
+    # the lower limit specified by the user (or the lowest period in the
+    # psd)
+    per_left = (period_limits[0] /
+                (period_smoothing_width_factor ** 0.5))
+    per_right = per_left * period_smoothing_width_factor
+    per_center = math.sqrt(per_left * per_right)
+
+    # build up lists
+    # per_octaves_left = [per_left]
+    # per_octaves_right = [per_right]
+    # per_octaves_center = [per_center]
+    previous_periods = per_left, per_center, per_right
+
+    idx = np.argwhere(psd_periods > per_center)[0][0]
+    psdlen = len(psd_periods)
+
     # do this for the whole period range and append the values to our lists
-    for per_left, per_right in zip(self.period_bin_left_edges,
-                                   self.period_bin_right_edges):
-        specs = spec[(per_left <= self.psd_periods) &
-                     (self.psd_periods <= per_right)]
-        smoothed_psd.append(specs.mean())
-    smoothed_psd = np.array(smoothed_psd, dtype=np.float32)
-    self.__insert_processed_data(tr.stats.starttime, smoothed_psd)
-    return True
+    while per_center < period_limits[1] and idx < psdlen:
+        # move left edge of smoothing bin further
+        per_left *= period_step_factor
+        # determine right edge of smoothing bin
+        per_right = per_left * period_smoothing_width_factor
+        # determine center period of smoothing/binning
+        per_center = math.sqrt(per_left * per_right)
+        # yield if:
+        if previous_periods[1] <= psd_periods[idx] and per_center >= psd_periods[idx]:
+            yield previous_periods
+            yield per_left, per_center, per_right
+            idx += 1
+
+        previous_periods = per_left, per_center, per_right
+
+
+        # append to lists
+#         per_octaves_left.append(per_left)
+#         per_octaves_right.append(per_right)
+#         per_octaves_center.append(per_center)
+#     per_octaves_left = np.array(per_octaves_left)
+#     per_octaves_right = np.array(per_octaves_right)
+#     per_octaves_center = np.array(per_octaves_center)
+#     valid = per_octaves_right > psd_periods[0]
+#     valid &= per_octaves_left < psd_periods[-1]
+#     per_octaves_left = per_octaves_left[valid]
+#     per_octaves_right = per_octaves_right[valid]
+#     per_octaves_center = per_octaves_center[valid]
+#     _period_binning = np.vstack([
+#         # left edge of smoothing (for calculating the bin value from psd
+#         per_octaves_left,
+#         # left xedge of bin (for plotting)
+#         per_octaves_center / (period_step_factor ** 0.5),
+#         # bin center (for plotting)
+#         per_octaves_center,
+#         # right xedge of bin (for plotting)
+#         per_octaves_center * (period_step_factor ** 0.5),
+#         # right edge of smoothing (for calculating the bin value from psd
+#         per_octaves_right])
+#     return _period_binning
+
+
+if __name__ == "__main__":
+    from os.path import dirname, join
+    trace, inv = 'trace_GE.APE.mseed', 'inventory_GE.APE.xml'
+    stream = read(join(dirname(__file__), 'miniseed', trace))
+    inv = read_inventory(join(dirname(__file__), 'miniseed', inv))
+    periods = [5] # [0.2, 2, 5, 9]
+    print(f'Periods to calculate on test miniseed: {periods}')
+    t = time.time()
+    _ = old.psd_values(periods, stream[0], inv)
+    print(f'Old method, values: {str(_)}, time: {time.time()-t}')
+    t = time.time()
+    _ = psd_values(periods, stream[0], inv)
+    print(f'New method, values: {str(_)}, time: {time.time()-t}')
+    
